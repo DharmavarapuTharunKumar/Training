27b43b6672655ff4a69acfdf9f1acf49f5908f06
DharmavarapuTharunKumar <152180271+DharmavarapuTharunKumar@users.noreply.github.com>
2024-04-22 12:04:08 +0530

    Update average.java

--- Daily Assignments/average.java
+++ Daily Assignments/average.java
@@ -1,1 +1,18 @@
+package com;
 
+public class Average {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] Array= {12,5,6,7,8,9};
+		int x=find_average(Array);
+		System.out.println(x);
+
+	}
+	public static int find_average(int[] Array) {
+		int average=0;
+		int sum=12+5+6+7+8+9;
+	    average=sum/Array.length;  
+		return average;
+	}
+}
