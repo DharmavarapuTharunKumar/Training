c4c11a2fcf440397be005454bd0de26ad4aea416
DharmavarapuTharunKumar <152180271+DharmavarapuTharunKumar@users.noreply.github.com>
2024-04-22 12:03:24 +0530

    Update arraysum.java

--- Daily Assignments/arraysum.java
+++ Daily Assignments/arraysum.java
@@ -1,1 +1,23 @@
+package com;
 
+public class ArraySum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[]= {1,6,5,7};
+		SumOfarray(a, a.length);
+		
+	}
+	public static void SumOfarray(int[] a,int length) {
+		int sum=0;
+		for(int i=0;i<a.length;i++)
+		{
+						
+			sum=sum+a[i];
+		}
+		System.out.println(sum);
+		
+
+	}
+
+}
